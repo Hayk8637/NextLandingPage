3be7563f5c7b02021f81b2e741e19b2d086fce59
Hayk8637 <62342210+Hayk8637@users.noreply.github.com>
2024-08-03 16:34:22 +0400

    last update

--- app/components/banner/Banner.tsx
+++ app/components/banner/Banner.tsx
@@ -2,6 +2,9 @@ import style from './style.module.css'
 const Banner:React.FC = () => {
     return <div className={style.banner}>
             <div className={style.text}>
+                <h1>click to see menu example</h1>
+            </div>
+            <div className={style.img}>
 
             </div>
         </div>;
