00e045b376170b53f4cc96709633e74a387657d4
Hayk8637 <62342210+Hayk8637@users.noreply.github.com>
2024-08-08 16:35:04 +0400

    a

--- app/components/aboutC/AboutC.tsx
+++ app/components/aboutC/AboutC.tsx
@@ -12,26 +12,41 @@ const AboutC: React.FC = () => {
     useEffect(() => {
         const savedLanguage = localStorage.getItem('language'); 
         if (savedLanguage) {
-          i18n.changeLanguage(savedLanguage); 
+            i18n.changeLanguage(savedLanguage); 
         }
-      }, [i18n]);
-      const aboutTexts = t('about', { returnObjects: true });
-    return<>
-    <div className={style.about}>
-    <div className={style.left}>
-        <Image className={style.img} src="/img/photo_2024-07-28_21-33-23.jpg" width={500} height={500} alt='img'/>
-    </div>
-    <div className={style.right}>
-        <div className={style.aboutData}>
-            <h2 className={style.aboutTitle}>{t("aboutTitle")}</h2>
-                {Object.values(aboutTexts).map((text, index) => (
-                    <p className={style.aboutParagraph} key={index}>{text}</p>
-                ))}      
+    }, [i18n]);
+
+    const aboutTextsArray = Object.values(t('AboutApp', { returnObjects: true })) as Array<string | Record<string, string>>;
+
+    return (
+        <div className={style.about}>
+            <div className={style.left}>
+                <Image 
+                    className={style.img} 
+                    src="/img/photo_2024-07-28_21-33-23.jpg" 
+                    width={500} 
+                    height={500} 
+                    alt='img'
+                />
+            </div>
+            <div className={style.right}>
+                <div className={style.aboutData}>
+                    <h2 className={style.aboutTitle}>{t("aboutTitle")}</h2>
+                    {aboutTextsArray.map((text, index) => (
+                        typeof text === 'string' ? (
+                            <p className={style.aboutParagraph} key={index}>{text}</p>
+                        ) : (
+                            <div key={index}>
+                                {Object.entries(text).map(([key, value]) => (
+                                    <p className={style.aboutParagraph} key={key}>{value}</p>
+                                ))}
+                            </div>
+                        )
+                    ))}      
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-      
-    </>
+    );
 }
 
 export default AboutC;
