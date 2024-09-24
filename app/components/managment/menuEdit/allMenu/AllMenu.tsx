5935214b6898f60798c8d5d2718309c6c9f4dfbc
Hayk8637 <62342210+Hayk8637@users.noreply.github.com>
2024-09-24 11:11:00 +0400

    any updates

--- app/components/managment/menuEdit/allMenu/AllMenu.tsx
+++ app/components/managment/menuEdit/allMenu/AllMenu.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Modal, Form, Input, Upload, Button, message } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
-import { collection, addDoc, getDocs, doc, updateDoc, getDoc } from 'firebase/firestore'; 
+import { Modal, Form, Input, Upload, Button, message, Popover, Switch } from 'antd';
+import { SmallDashOutlined, UploadOutlined } from '@ant-design/icons';
+import { doc, updateDoc, getDoc, deleteField } from 'firebase/firestore';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
-import { auth, db, storage } from '../../../../../firebaseConfig'; 
-import style from './style.module.css'
+import { auth, db, storage } from '../../../../../firebaseConfig';
+import style from './style.module.css';
 import { usePathname } from 'next/navigation';
-import { url } from 'inspector';
 
 interface MenuCategoryItem {
   id: string;
   name: string;
-  imgUrl: string | null; 
+  imgUrl: string | null;
+  isVisible: boolean;
 }
 
 const AllMenu: React.FC = () => {
@@ -19,9 +19,11 @@ const AllMenu: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
   const [newCategory, setNewCategory] = useState<{ name: string, imgUrl: string | null }>({ name: '', imgUrl: null });
   const [imageFile, setImageFile] = useState<File | null>(null);
   const [uploading, setUploading] = useState(false);
+  const [currentEditingId, setCurrentEditingId] = useState<string | null>(null);
 
   const pathname = usePathname() || '';
   const establishmentId = pathname.split('/').filter(Boolean).pop() || '';
@@ -31,21 +33,20 @@ const AllMenu: React.FC = () => {
     const fetchMenuItems = async () => {
       if (userId && establishmentId) {
         try {
-          // Get the specific establishment document
           const docRef = doc(db, 'users', userId, 'establishments', establishmentId);
           const docSnap = await getDoc(docRef);
-  
+
           if (docSnap.exists()) {
             const data = docSnap.data();
-            const categories = data.menu?.categories || {}; // Retrieve categories from menu
-  
-            // Convert the categories object into an array of MenuCategoryItem
+            const categories = data.menu?.categories || {};
+
             const items: MenuCategoryItem[] = Object.entries(categories).map(([id, category]: any) => ({
               id,
               name: category.name,
               imgUrl: category.imgUrl,
+              isVisible: category.isVisible ?? true,
             }));
-  
+
             setMenuItems(items);
           } else {
             setError('No categories found');
@@ -57,19 +58,30 @@ const AllMenu: React.FC = () => {
         }
       }
     };
-  
+
     fetchMenuItems();
   }, [userId, establishmentId]);
-  
 
   const showModal = () => {
     setIsModalVisible(true);
   };
 
+  const showEditModal = (item: MenuCategoryItem) => {
+    setNewCategory({ name: item.name, imgUrl: item.imgUrl });
+    setCurrentEditingId(item.id);
+    setIsEditModalVisible(true);
+  };
+
   const handleCancel = () => {
     setIsModalVisible(false);
     setNewCategory({ name: '', imgUrl: null });
-    setImageFile(null); 
+    setImageFile(null);
+  };
+
+  const handleEditCancel = () => {
+    setIsEditModalVisible(false);
+    setNewCategory({ name: '', imgUrl: null });
+    setCurrentEditingId(null);
   };
 
   const handleImageUpload = (file: File) => {
@@ -113,19 +125,17 @@ const AllMenu: React.FC = () => {
         });
       }
 
-      const uniqueId = Date.now().toString(); // Generate a unique ID for the new category
-      
-      // Update the specific document under 'establishments' with the new category data
+      const uniqueId = Date.now().toString();
       const docRef = doc(db, 'users', userId, 'establishments', establishmentId);
       await updateDoc(docRef, {
         [`menu.categories.${uniqueId}`]: {
           name: newCategory.name,
           imgUrl: imgUrl || null,
+          isVisible: true,
         },
       });
 
-      // Update local state
-      setMenuItems((prev) => [...prev, { id: uniqueId, name: newCategory.name, imgUrl }]);
+      setMenuItems((prev) => [...prev, { id: uniqueId, name: newCategory.name, imgUrl, isVisible: true }]);
       message.success('Category created successfully');
       handleCancel();
     } catch (error) {
@@ -135,21 +145,122 @@ const AllMenu: React.FC = () => {
     }
   };
 
+  const handleEditSubmit = async () => {
+    if (!newCategory.name) {
+      message.error('Category name is required');
+      return;
+    }
+
+    if (!userId || !establishmentId || !currentEditingId) {
+      message.error('Missing user or establishment information');
+      return;
+    }
+
+    setUploading(true);
+    try {
+      const docRef = doc(db, 'users', userId, 'establishments', establishmentId);
+      await updateDoc(docRef, {
+        [`menu.categories.${currentEditingId}`]: {
+          name: newCategory.name,
+          imgUrl: newCategory.imgUrl,
+          isVisible: true,
+        },
+      });
+
+      const updatedItems = menuItems.map(item => 
+        item.id === currentEditingId ? { ...item, name: newCategory.name, imgUrl: newCategory.imgUrl } : item
+      );
+      setMenuItems(updatedItems);
+      message.success('Category updated successfully');
+      handleEditCancel();
+    } catch (error) {
+      message.error(`Error updating category: ${error}`);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const handleToggleVisibility = async (id: string, isVisible: boolean) => {
+    if (userId && establishmentId) {
+      try {
+        const updatedItems = menuItems.map(item => 
+          item.id === id ? { ...item, isVisible } : item
+        );
+        setMenuItems(updatedItems);
+
+        const categoryRef = doc(db, 'users', userId, 'establishments', establishmentId);
+        await updateDoc(categoryRef, {
+          [`menu.categories.${id}.isVisible`]: isVisible,
+        });
+        message.info(`Visibility toggled for category ${id} to ${isVisible ? 'ON' : 'OFF'}`);
+      } catch (error) {
+        message.error(`Error toggling visibility: ${error}`);
+      }
+    } else {
+      message.error('User ID or establishment ID is missing');
+    }
+  };
+
+  const handleDelete = async (id: string) => {
+    if (userId && establishmentId) {
+      try {
+        const updatedItems = menuItems.filter(item => item.id !== id);
+        setMenuItems(updatedItems);
+
+        const categoryRef = doc(db, 'users', userId, 'establishments', establishmentId);
+        await updateDoc(categoryRef, {
+          [`menu.categories.${id}`]: deleteField(),
+        });
+
+        message.success(`Category ${id} deleted successfully`);
+      } catch (error) {
+        message.error(`Error deleting category: ${error}`);
+      }
+    } else {
+      message.error('User ID or establishment ID is missing');
+    }
+  };
+
+  const popoverContent = (item: MenuCategoryItem) => (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Button onClick={(e) => { e.stopPropagation(); showEditModal(item); }} style={{ marginBottom: 8 }}>Edit</Button>
+      <div style={{ marginBottom: 8 }}>
+        <span>Visibility:</span>
+        <Switch checked={item.isVisible} onChange={(checked) => handleToggleVisibility(item.id, checked)} />
+      </div>
+      <Button onClick={(e) => { e.stopPropagation(); handleDelete(item.id); }}>Delete</Button>
+    </div>
+  );
 
   return (
     <div className={style.allMenu}>
       <div className={style.menuCategories}>
-        {menuItems.map(item => (
-          <button
-            key={item.id}
-            className={style.menuCategoryItem}
-            style={{ backgroundImage: `url(${item.imgUrl || ''})` }}
-            onClick={() => window.location.href = `/MENUBYQR/menu/${item.name}`}
-          >
-            <span>{item.name}</span>
+      {menuItems.map(item => (
+      <button
+        key={item.id}
+        className={style.menuCategoryItem}
+        style={{ backgroundImage: `url(${item.imgUrl || ''})` }}
+        onClick={(e) => {
+          // Check if the click event originated from this button
+          if (e.currentTarget === e.target) {
+            window.location.href = `/MENUBYQR/menu/${item.name}`;
+          }
+        }}
+      >
+        <span>{item.name}</span>
+        <Popover
+          content={popoverContent(item)}
+          trigger="hover"
+          placement="topRight"
+        >
+          <button className={style.functions} onClick={(e) => e.stopPropagation()}>
+            <SmallDashOutlined />
           </button>
-        ))}
-        
+        </Popover>
+      </button>
+    ))}
+
+
         <button className={style.menuCategoryItem} onClick={showModal}>
           <span>+</span>
         </button>
@@ -159,26 +270,50 @@ const AllMenu: React.FC = () => {
         title="Create New Category"
         visible={isModalVisible}
         onCancel={handleCancel}
-        onOk={handleSubmit}
-        confirmLoading={uploading}
+        footer={null}
       >
         <Form layout="vertical">
           <Form.Item label="Category Name" required>
             <Input
               value={newCategory.name}
-              onChange={e => setNewCategory({ name: e.target.value, imgUrl: newCategory.imgUrl })}
-              placeholder="Enter category name"
+              onChange={(e) => setNewCategory({ ...newCategory, name: e.target.value })}
             />
           </Form.Item>
-          <Form.Item label="Category Image (Optional)">
-            <Upload
-              beforeUpload={handleImageUpload}
-              accept="image/*"
-              showUploadList={false}
-            >
-              <Button icon={<UploadOutlined />}>Select Image</Button>
+          <Form.Item label="Image Upload">
+            <Upload beforeUpload={handleImageUpload} showUploadList={false}>
+              <Button icon={<UploadOutlined />}>Upload</Button>
             </Upload>
-            {imageFile && <span>{imageFile.name}</span>}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" loading={uploading} onClick={handleSubmit}>
+              Create
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      <Modal
+        title="Edit Category"
+        visible={isEditModalVisible}
+        onCancel={handleEditCancel}
+        footer={null}
+      >
+        <Form layout="vertical">
+          <Form.Item label="Category Name" required>
+            <Input
+              value={newCategory.name}
+              onChange={(e) => setNewCategory({ ...newCategory, name: e.target.value })}
+            />
+          </Form.Item>
+          <Form.Item label="Image Upload">
+            <Upload beforeUpload={handleImageUpload} showUploadList={false}>
+              <Button icon={<UploadOutlined />}>Upload</Button>
+            </Upload>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" loading={uploading} onClick={handleEditSubmit}>
+              Update
+            </Button>
           </Form.Item>
         </Form>
       </Modal>
