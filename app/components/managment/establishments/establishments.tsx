22c996b49b2a4c977bed5f6e8246191bf0d8c7ea
Hayk8637 <62342210+Hayk8637@users.noreply.github.com>
2024-10-04 14:37:50 +0400

    any updates

--- app/components/managment/establishments/establishments.tsx
+++ app/components/managment/establishments/establishments.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, use } from 'react';
-import { Form, Input, Button, notification, Modal, Popconfirm, Popover } from 'antd';
-import { FileAddOutlined, DeleteOutlined, EditOutlined, LinkOutlined, QrcodeOutlined, SkinOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react';
+import { Form, Input, Button, notification, Modal, Popconfirm, Popover, QRCode } from 'antd';
+import { FileAddOutlined, DeleteOutlined, EditOutlined, QrcodeOutlined } from '@ant-design/icons';
 import { getFirestore, collection, addDoc, query, where, onSnapshot, doc, deleteDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
@@ -8,6 +8,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './style.module.css';
+
 interface Establishment {
   id?: string;
   info: {
@@ -25,6 +26,7 @@ interface Establishment {
   };
   uid: string;
 }
+
 const Establishments: React.FC = () => {
   const [form] = Form.useForm();
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -32,17 +34,17 @@ const Establishments: React.FC = () => {
   const [isQrLinkModalVisible, setIsQrLinkModalVisible] = useState(false);
   const [establishments, setEstablishments] = useState<Establishment[]>([]);
   const [bannerFiles, setBannerFiles] = useState<File[]>([]);
+  const [selectedEstablishmentId, setSelectedEstablishmentId] = useState<string | null>(null);
   const router = useRouter();
-
   const auth = getAuth();
   const db = getFirestore();
   const storage = getStorage();
   const user = auth.currentUser;
 
   useEffect(() => {
-    if(user) {
+    if (user) {
       const fetchEstablishments = async () => {
-        const userId = user?.uid; 
+        const userId = user?.uid;
         if (userId) {
           const q = query(collection(db, 'users', userId, 'establishments'), where('uid', '==', userId));
           const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -57,8 +59,9 @@ const Establishments: React.FC = () => {
         }
       };
       fetchEstablishments();
-    }  
-  }, [user , db]);
+    }
+  }, [user, db]);
+
   const handleAddEstablishment = async (values: any) => {
     try {
       const user = auth.currentUser;
@@ -70,9 +73,7 @@ const Establishments: React.FC = () => {
           const url = await getDownloadURL(snapshot.ref);
           bannerUrls.push(url);
         });
-
         await Promise.all(uploadPromises);
-
         const establishmentData: Establishment = {
           info: {
             name: values.name,
@@ -90,26 +91,36 @@ const Establishments: React.FC = () => {
           uid: user.uid,
         };
         const docRef = await addDoc(collection(db, 'users', user.uid, 'establishments'), establishmentData);
-        notification.success({
-          message: 'Establishment Added',
-        });
+        notification.success({ message: 'Establishment Added' });
         form.resetFields();
         setBannerFiles([]);
         handleModalClose();
-        router.push(`/profile/establishments/${docRef.id}`); 
+        router.push(`/profile/establishments/${docRef.id}`);
       }
-    } catch (error: any) {
+    } catch (error) {
+      console.error('Error adding establishment:', error);
     }
   };
 
+  const handleCopyLink = () => {
+    const linkToCopy = `https://menu.menubyqr.com/${user?.uid}/${selectedEstablishmentId}`;
+    navigator.clipboard.writeText(linkToCopy)
+      .then(() => {
+        notification.success({ message: 'Link copied to clipboard!' });
+      })
+      .catch((error) => {
+        console.error('Failed to copy the link: ', error);
+        notification.error({ message: 'Failed to copy the link', description: error.message });
+      });
+  };
   const handleDeleteEstablishment = async (id: string) => {
-    setIsQrLinkModalVisible(false)
+    setIsQrLinkModalVisible(false);
     setIsStylesModalVisible(false);
-      const user = auth.currentUser;
-      if (user) {
-        const docRef = doc(db, 'users', user.uid, 'establishments', id);
-        await deleteDoc(docRef);
-      }
+    const user = auth.currentUser;
+    if (user) {
+      const docRef = doc(db, 'users', user.uid, 'establishments', id);
+      await deleteDoc(docRef);
+    }
   };
 
   const handleModalOpen = () => {
@@ -122,7 +133,7 @@ const Establishments: React.FC = () => {
   };
 
   const handleStylesModalOpen = () => {
-    setIsQrLinkModalVisible(false)
+    setIsQrLinkModalVisible(false);
     setIsStylesModalVisible(true);
   };
 
@@ -130,9 +141,10 @@ const Establishments: React.FC = () => {
     setIsStylesModalVisible(false);
   };
 
-  const handleQrLinkModalOpen = () => {
+  const handleQrLinkModalOpen = (id: string) => {
     setIsStylesModalVisible(false);
     setIsQrLinkModalVisible(true);
+    setSelectedEstablishmentId(id);
   };
 
   const handleQrLinkModalClose = () => {
@@ -151,27 +163,27 @@ const Establishments: React.FC = () => {
         {establishments.map((establishment) => (
           <div className={styles.establishmentContainer} key={establishment.id}>
             <Link className={styles.link} href={`/profile/establishments/${establishment.id}`}>
-            <Button className={styles.establishmentButton}>
-              <span>
-                <Image
-                  src={establishment.info.logoUrl || './MBQR Label-03.png'}
-                  alt="Establishment Logo"
-                  className={styles.logoImage}
-                  priority
-                  width={100}
-                  height={100}
-                  style={{ objectFit: 'contain' }} // Inline style for object fit
-                />
-              </span>
-            </Button>
+              <Button className={styles.establishmentButton}>
+                <span>
+                  <Image
+                    src={establishment.info.logoUrl || './MBQR Label-03.png'}
+                    alt="Establishment Logo"
+                    className={styles.logoImage}
+                    priority
+                    width={100}
+                    height={100}
+                    style={{ objectFit: 'contain' }}
+                  />
+                </span>
+              </Button>
             </Link>
             <Popover
               content={
                 <div>
-                  <Button className={styles.editButtons}  onClick={handleStylesModalOpen}>
+                  <Button className={styles.editButtons} onClick={handleStylesModalOpen}>
                     Menu Styles
                   </Button>
-                  <Button className={styles.editButtons} icon={<QrcodeOutlined />} onClick={handleQrLinkModalOpen}>
+                  <Button className={styles.editButtons} icon={<QrcodeOutlined />} onClick={() => handleQrLinkModalOpen(establishment.id!)}>
                     QR or Link
                   </Button>
                   <Popconfirm
@@ -188,11 +200,7 @@ const Establishments: React.FC = () => {
               }
               trigger="click"
             >
-              <Button
-                type="link"
-                icon={<EditOutlined />}
-                className={styles.editButton}
-              />
+              <Button type="link" icon={<EditOutlined />} className={styles.editButton} />
             </Popover>
           </div>
         ))}
@@ -205,18 +213,8 @@ const Establishments: React.FC = () => {
         </Button>
       </div>
 
-      {/* Modal for adding establishment */}
-      <Modal
-        title="Add Establishment"
-        open={isModalVisible}
-        onCancel={handleModalClose}
-        footer={null}
-      >
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={handleAddEstablishment}
-        >
+      <Modal title="Add Establishment" open={isModalVisible} onCancel={handleModalClose} footer={null}>
+        <Form form={form} layout="vertical" onFinish={handleAddEstablishment}>
           <Form.Item
             label="Establishment Name"
             name="name"
@@ -229,36 +227,38 @@ const Establishments: React.FC = () => {
             Add Establishment
           </Button>
 
-          <Button
-            style={{ marginTop: '10px', width: '100%' }}
-            onClick={handleModalClose}
-          >
+          <Button style={{ marginTop: '10px', width: '100%' }} onClick={handleModalClose}>
             Cancel
           </Button>
         </Form>
       </Modal>
 
-      {/* Modal for Styles */}
-      <Modal
-        title="Edit Styles"
-        open={isStylesModalVisible}
-        onCancel={handleStylesModalClose}
-        footer={null}
-      >
-        {/* Styles form content */}
-        <p>Styles modal content goes here...</p>
+      <Modal title="Edit Styles" open={isStylesModalVisible} onCancel={handleStylesModalClose} footer={null}>
+          here i must edit my styles 
       </Modal>
 
-      {/* Modal for QR or Link */}
-      <Modal
-        title="QR or Link"
-        open={isQrLinkModalVisible}
-        onCancel={handleQrLinkModalClose}
-        footer={null}
-      >
-        {/* QR or Link form content */}
-        <p>QR or Link modal content goes here...</p>
-      </Modal>
+   <Modal title="QR or Link" open={isQrLinkModalVisible} onCancel={handleQrLinkModalClose} footer={null}>
+  {selectedEstablishmentId && (
+    <div className={styles.qrlink}>
+      <div className={styles.qr}>
+        <QRCode
+          className={styles.qrcode}
+          errorLevel="H"
+          value={`https://menu.menubyqr.com/${user?.uid}/${selectedEstablishmentId}`}/>
+      </div>
+      <div className={styles.link}>
+        <p>QR Link:</p>
+        <Link className={styles.linklink} href={`https://menu.menubyqr.com/${user?.uid}/${selectedEstablishmentId}`}>
+          {`https://menu.menubyqr.com/${user?.uid}/${selectedEstablishmentId}`}
+        </Link>
+        <Button type="primary" className={styles.qrlinkbutton} onClick={handleCopyLink}>
+          copy menu link
+        </Button>
+      </div>
+    </div>
+  )}
+</Modal>
+
     </div>
   );
 };
